91d51fdb4561a1d38b79ac18935ec42ce54d1c9e
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2021-06-17 12:12:10 +0200

    main

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,6 +1,21 @@
-sentense = "I am a man who not is good in coding"
+import subprocess
+import sys
+try:
+    import nltk
+    
+except ModuleNotFoundError:
+    print("Warning: nltk module not found!")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'nltk'])
+    import nltk
+
+if not 'averaged_perceptron_tagger':
+    nltk.download('averaged_perceptron_tagger')
+
+sentense = "i told Sanchayan what is my name"
+
 
 class Subject:
+
     def __init__(self):
         pass
     
@@ -9,23 +24,18 @@ class Subject:
     def changeSentiment(self, new_sentiment):
         self.sentiment = new_sentiment
 
-def classifier(sentense, point_of_view):
-    import subprocess
-    import sys
-    try:
-        import nltk
-    
-    except ModuleNotFoundError:
-        print("Warning: nltk module not found!")
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'nltk'])
-        import nltk
 
-    if not 'averaged_perceptron_tagger':
-        nltk.download('averaged_perceptron_tagger')
+def classifier(sentense, point_of_view):
 
     tokenized_sentense = nltk.word_tokenize(sentense)
     pos_tags = nltk.pos_tag(tokenized_sentense)
+    dict_of_subjects = dict()
 
-    return pos_tags
+    for word_tags in pos_tags:
+        if word_tags[1] in ("NN","NNS", "NNP", "NNPS"):
+            dict_of_subjects[word_tags[0]] = Subject()    
 
-print(classifier(sentense,0))
+
+    return(dict_of_subjects[point_of_view].sentiment)
+
+print(classifier(sentense,'Sanchayan'))
